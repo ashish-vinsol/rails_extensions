74d716a6bbcdcd784edaa31b0f9f9bbb8661ace2
Sawan Gupta <sawan.gupta92@gmail.com>
2015-12-10 23:36:05 +0530

    Added CR comments for products.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,28 @@
 class Product < ActiveRecord::Base
+  #FIXME_SG: spacing
+  #FIXME_SG: tell me the query 'product.line_item' will fire.
   has_many :line_items ,dependent: :restrict_with_error
   has_many :carts, through: :line_items
   belongs_to :category
+  #FIXME_SG: remove if not using
   # before_destroy :ensure_not_referenced_by_any_line_item
+
+  #FIXME_SG: arrange them in order they will be executed.
   before_validation :check_title, :set_discount_price
 
   after_save :update_products_count
 
   validates :title, :description, :image_url, presence: true
+  #FIXME_SG: spacing
   validates :price, numericality: {greater_than_or_equal_to: 0.01}, allow_blank: true
   validates_with PriceValidator
   validates :title, uniqueness: true
+  #FIXME_SG: why allow_blank true?
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }, url: true
+  #FIXME_SG: either use presence true or allow_blank acc to requirment.
   validates :permalink, uniqueness: true, format: {
     with: /\A(\w+-){2,}\w+\Z/,
     message: 'wrong format'
@@ -24,7 +32,7 @@ class Product < ActiveRecord::Base
   scope :enabled, -> { where(enabled: true) }
 
   private
-
+  #FIXME_SG: remove if not using
   def ensure_not_referenced_by_any_line_item
     if line_items.empty?
       return true
@@ -39,15 +47,24 @@ class Product < ActiveRecord::Base
   end
 
   #FIXME: discuss with ...
+  #FIXME_SG: Naming
   def check_title
+    #FIXME_SG: you can also write it like
+    # => self.title = 'abc' if self.title.blank?
     self.title.blank? ? (self.title = 'abc') : nil
   end
 
   def set_discount_price
+    #FIXME_SG: you can also write it like
+    # => self.discount_price = self.price if self.discount_price.blank?
     self.discount_price.blank? ? (self.discount_price = self.price) : nil
   end
 
   def update_products_count
+    #FIXME_SG: spacing
+    #FIXME_SG: why are we doing Category.find(self.category_id)
+    # => first we do not have to write self
+    # => i think category will also return the same thing confirm.
     Category.find(self.category_id).update_column(count:  Category.find(self.category_id).products.size)
   end
 
